d647c49f62204b47fb24eeabfeb04b1040352357
andela-nmartin <godmetweenciati@gmail.com>
2016-01-08 16:07:59 +0300

    [#111131880] Renames the server configuration file

--- server/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'database': process.env.DATABASE || 'mongodb://localhost/docms',
-  'port': process.env.PORT || 3000,
-  'secretKey': process.env.SECRET_KEY || 'YoUrS3cr3tK3y',
-  'codeClimateRepoToken': process.env.CODECLIMATE_REPO_TOKEN || 
-  '568f4a1796095db71636f201fa72ea5d7dd7c9d85ffd506fd0e78e9c3cc1cd94' 
-};
