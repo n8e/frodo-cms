1f9fdaab4e7a4dee243b28bc101ac82e700c13de
andela-nmartin <godmetweenciati@gmail.com>
2016-01-19 11:24:43 +0300

    [#110907918] Breaks long line causing linting error

--- tests/unit/client/controllers/document.spec.js
+++ tests/unit/client/controllers/document.spec.js
@@ -20,33 +20,34 @@ describe('Controller: DocumentController', function() {
       expect(controller.documents).toBeDefined();
       expect(controller.documents).toBe('data');
     });
-    it('createDocument is a function and should call Document.create', function() {
-      Document.create = sinon.spy();
-      controller.docData = {
-        title: 'title',
-        content: 'content'
-      };
-      controller.createDocument();
-      expect(Document.create.called).toBe(true);
-      expect(typeof Document.create).toBe('function');
-      expect(controller.processing).toBe(true);
-      expect(controller.docData).toBeDefined();
-      expect(typeof controller.docData).toBe('object');
-      $location.path = sinon.stub();
-      Document.create.args[0][1]({
-        success: true,
-        message: 'message'
+    it('createDocument is a function and should call Document.create',
+      function() {
+        Document.create = sinon.spy();
+        controller.docData = {
+          title: 'title',
+          content: 'content'
+        };
+        controller.createDocument();
+        expect(Document.create.called).toBe(true);
+        expect(typeof Document.create).toBe('function');
+        expect(controller.processing).toBe(true);
+        expect(controller.docData).toBeDefined();
+        expect(typeof controller.docData).toBe('object');
+        $location.path = sinon.stub();
+        Document.create.args[0][1]({
+          success: true,
+          message: 'message'
+        });
+        expect(controller.processing).toBe(false);
+        expect(controller.message).not.toBe('');
+        expect($location.path.called).toBe(true);
+        Document.create.args[0][1]({
+          success: false,
+          message: 'message'
+        });
+        expect($location.path.calledOnce).toBe(true);
+        expect(controller.error).toBeDefined();
+        expect(controller.error).toBe('message');
       });
-      expect(controller.processing).toBe(false);
-      expect(controller.message).not.toBe('');
-      expect($location.path.called).toBe(true);
-      Document.create.args[0][1]({
-        success: false,
-        message: 'message'
-      });
-      expect($location.path.calledOnce).toBe(true);
-      expect(controller.error).toBeDefined();
-      expect(controller.error).toBe('message');
-    });
   });
 });
