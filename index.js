beb2443474d95496eae433a9ff6c2337afa82cde
andela-nmartin <godmetweenciati@gmail.com>
2016-01-20 11:26:39 +0300

    [#111131880] Removes socketio from express server

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ var mongoose = require('mongoose');
 var morgan = require('morgan');
 var app = express();
 
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-
 // connect to Mongo when the app initializes and 
 // drop the db before seeding
 mongoose.connect(config.database, function(err) {
