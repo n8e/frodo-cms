be2e4f8acb39e7ce3fb33913dc8ec81699d87d0a
andela-nmartin <godmetweenciati@gmail.com>
2016-01-29 15:16:22 +0300

    [#112414177] Modifies delete docs for admin

--- app/scripts/controllers/all.documents.js
+++ app/scripts/controllers/all.documents.js
@@ -3,8 +3,8 @@
     .controller('AllDocumentsController', ['$rootScope', 'Document', 'Auth',
       function($rootScope, Document, Auth) {
         var self = this;
-        self.documents = '';
-        self.allDocuments = '';
+        self.documents = [];
+        self.allDocuments = [];
 
         $rootScope.$on('$routeChangeStart', function() {
           // self.loggedIn = Auth.isLoggedIn();
@@ -36,6 +36,10 @@
             self.processing = false;
             if (data.message._id) {
               var documentRemovedID = data.message._id;
+              // for all the documents
+              self.allDocuments = self.allDocuments.filter(function(doc) {
+                return doc._id !== documentRemovedID;
+              });
               // a new array for the documents
               self.documents = self.documents.filter(function(doc) {
                 return doc._id !== documentRemovedID;
