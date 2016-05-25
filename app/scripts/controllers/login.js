ee4a05bafe1f1f38433e856dcd07a20d8d59e73e
andela-nmartin <godmetweenciati@gmail.com>
2016-05-25 13:55:13 +0300

    fixing breaking route

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -5,7 +5,6 @@
       function($rootScope, $location, Auth) {
         var self = this;
         self.loggedIn = Auth.isLoggedIn();
-
         $rootScope.$on('$routeChangeStart', function() {
           self.loggedIn = Auth.isLoggedIn();
           Auth.getUser(function(err, data) {
@@ -21,6 +20,7 @@
             self.processing = false;
             Auth.getUser(function(err, data) {
               self.user = data;
+              $rootScope.currentUser = data;
             });
             if (data.success) {
               $location.path('/profile');
